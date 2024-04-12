905c639db3239c319e85d4d4dbdf73ec74fc7de5
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-12 11:28:46 -0700

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+string dogName = "Rex";
+int age = 5;
+char middleInitial = 'S';
+bool isOver2 = true;
+double currentTemp = 78;
+decimal currentPrice = 1875.55m;
+
+Console.WriteLine($"Hello, my dog's name is {dogName} and he is {age} years old.  His middle initial is {middleInitial}");
+Console.WriteLine($"Is he over 2 years old? {isOver2}!  The current temp is {currentTemp} degrees F and the dog cost ${currentPrice}");
